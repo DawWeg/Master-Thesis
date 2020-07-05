ec1a0fe43cf0874bcaac52832e5cb8c14c18c02e
Theyw <daw.weg95@gmail.com>
2020-07-05 15:12:04 +0200

    finished restructurizing unidirectional methods

--- main.m
+++ main.m
@@ -14,21 +14,21 @@ addpath("methods");
 should_plot = 1;
 should_save_audio = 1;
 load_audio_start_second = 0;
-load_audio_end_second = 7; %-1 for whole file
+load_audio_end_second = 5; %-1 for whole file
 global output_directory="output_samples/";
 global ewls_lambda = 0.999;
 global ewls_lambda_0 = 0.998;
 global ewls_noise_variance_coupled = 1; % 1=coupled | other=decoupled
 global ewls_initial_cov_matrix = 100;
 global model_rank = 10;
-global mu = 4;
+global mu = 4.5;
 
 % Residual methods exclusive parameters
 block_size = 256;
 block_shift = 128;
 
 % Prediction methods exclusive parameters
-global max_corrupted_block_length = 50;
+global max_corrupted_block_length = 200;
 global detection_delay = 10*model_rank;
 global decimal_accuracy = 12;
 
@@ -39,7 +39,7 @@ filenames = [ ...
               "Chopin_Etiuda_Op_25_nr_10.WAV";...
               "12.wav" ...
             ]; 
-current_file = filenames(1,:);
+current_file = filenames(4,:);
 
 [input_signal, frequency] = load_audio(current_file, load_audio_start_second, load_audio_end_second);
 input_signal = input_signal(:,1);
@@ -53,11 +53,11 @@ input_signal = input_signal(:,1);
 %   R_U_S_T_RK_release_threshold  ] = R_U_S_T_RK(input_signal, block_size, block_shift);
 %R_U_S_T_RK_threshold = [R_U_S_T_RK_activate_threshold, R_U_S_T_RK_release_threshold];
   
-dbstop("P_U_S_O_RK");  
+dbstop("P_U_S_C_RK");  
 [  P_U_S_O_RK_output_signal,...
    P_U_S_O_RK_detection_signal,...
    P_U_S_O_RK_prediction_errors,...
-   P_U_S_O_RK_activate_threshold  ] = P_U_S_O_VK(input_signal);   
+   P_U_S_O_RK_activate_threshold  ] = P_U_S_C_RK(input_signal);   
 %%% Plotting results 
 if should_plot
   %plot_result(1, input_signal, R_U_S_T_RK_detection_signal, R_U_S_T_RK_output_signal);
@@ -70,7 +70,7 @@ endif
 %%% Saving audio files
 if should_save_audio
   %save_audio(current_file, R_U_S_T_RK_output_signal, frequency, 1);
-  save_audio(current_file, P_U_S_O_RK_output_signal, frequency, 1);
+  save_audio(current_file, "P_U_S_O_RK", P_U_S_O_RK_output_signal, frequency, 1);
 endif
 
 
---
+++ methods/P_U_S_C_RK.m
@@ -0,0 +1,109 @@
+function [  clear_signal,...
+            detection_signal,...
+            error_trajectory,...
+            error_threshold  ] = P_U_S_C_RK(input_signal)
+%%% Preparing variables
+global model_rank ewls_lambda ewls_lambda_0 ewls_initial_cov_matrix mu max_corrupted_block_length detection_delay;
+N = length(input_signal);
+clear_signal = input_signal;
+covariance_matrix = ewls_initial_cov_matrix*eye(model_rank);
+coefficients_trajectory = zeros(N, model_rank);
+regression_vector = zeros(model_rank, 1);
+noise_variance_trajectory = zeros(N, 1);
+detection_signal = zeros(N, 1);
+inv_lambda = 1/ewls_lambda;
+error_trajectory = zeros(N, 1);
+error_threshold = zeros(N, 1);
+counter = 0;
+
+%%% Corrupted samples detection loop
+t = 2;
+while(t <= N)
+    % Estimating model parameters using weighted recursive least squares algorithm
+    regression_vector = [clear_signal(t-1); regression_vector(1:end-1)];
+    [coefficients_trajectory(t,:), covariance_matrix, error_trajectory(t), noise_variance_trajectory(t)] = ...
+              ewls_step(clear_signal(t), ...
+              regression_vector, ...
+              covariance_matrix, ...
+              coefficients_trajectory(t-1,:)', ...
+              noise_variance_trajectory(t-1));
+    error_threshold(t) = mu*sqrt(noise_variance_trajectory(t-1)); 
+    if(t < detection_delay || t > N-model_rank-max_corrupted_block_length || counter > 0)
+      counter = counter - 1;
+      t = t + 1;
+      continue;
+    endif
+    % Checking if the sample is corrupted
+    if(abs(error_trajectory(t)) > error_threshold(t))
+      % If the sample is corrupted alarm is raised.
+      % We check the model stability, and if it's not stable we use Levinson-Durbin
+      % algorithm to make sure that it is.
+      % TODO Levinson-Durbin algorithm
+      if(!check_stability(coefficients_trajectory(t-1,:)))
+        disp("Model unstable");     
+      endif    
+      false_positive = 0;
+      detection_signal(t) = 1;
+      block_start_index = t;
+      kalman_state_vector = clear_signal(t:-1:t-model_rank+1);
+      kalman_covariance_matrix = zeros(model_rank);
+      kalman_coefficients = coefficients_trajectory(t-1,:);
+      for i = 1:(max_corrupted_block_length-1)
+        [kalman_state_vector, ...
+        kalman_covariance_matrix, ...
+        kalman_coefficients, ...
+        kalman_error, ...
+        kalman_noise_variance] = closed_loop_detector_step( ...
+                                 kalman_state_vector, ...
+                                 kalman_covariance_matrix, ...
+                                 kalman_coefficients, ...
+                                 noise_variance_trajectory(t-1,:), ...
+                                 clear_signal(t+i));       
+        if(abs(kalman_error) > mu*sqrt(kalman_noise_variance))
+          detection_signal(t+i) = 1;
+          kalman_state_vector = kalman_state_vector;
+          kalman_covariance_matrix = kalman_covariance_matrix;
+        else
+          kalman_l = mround((1/kalman_noise_variance)*kalman_covariance_matrix(:,1));         
+          kalman_state_vector = mround(kalman_state_vector + kalman_l*kalman_error);
+          kalman_covariance_matrix = mround(kalman_covariance_matrix - kalman_noise_variance*kalman_l*kalman_l');
+          if(max(detection_signal(t+i-model_rank+1:t+i)) != 0)
+            false_positive = 1;
+          endif          
+        endif    
+        if(max(detection_signal(t+i-model_rank+1:t+i)) == 0)
+          m = t + i - block_start_index - model_rank;
+          q = 2*model_rank + m;
+          detection_signal(block_start_index:block_start_index+m-1) = 1;
+          clear_signal(block_start_index:block_start_index+m-1) = recursive_interpolation( ...
+                  clear_signal(block_start_index-q:t+i),...
+                  m, ...
+                  q, ...
+                  coefficients_trajectory(t-1,:), ...
+                  noise_variance_trajectory(t-1));          
+          t = t-1;
+          counter = model_rank;
+          break;
+        elseif(i >= max_corrupted_block_length)
+          m = max_corrupted_block_length;
+          q = 2*model_rank + m;
+          detection_signal(block_start_index:t+m-1) = 1;
+          clear_signal(block_start_index:t+m) = recursive_interpolation( ...
+                  clear_signal(block_start_index-q:t+m+model_rank), ...
+                  m, ...
+                  q, ...
+                  coefficients_trajectory(t-1,:), ...
+                  noise_variance_trajectory(t-1));
+          t = t-1;  
+          counter = model_rank; 
+          break;       
+        endif                
+      endfor 
+    endif
+    if(mod(t,1000) == 0)
+      printf("[%*d|100]\n", 3, round((t/N)*100));
+    endif
+    t = t + 1;
+endwhile
+disp("[100|100]");
+endfunction
