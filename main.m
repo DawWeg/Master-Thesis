8b1156500dbdb73b6bd53b25959153a399eaacbb
Maciej Sterma <maciej.sterma@gmail.com>
2020-07-05 22:47:25 +0200

    Updated server script

--- main.m
+++ main.m
@@ -14,24 +14,23 @@ addpath("methods");
 should_plot = 1;
 should_save_audio = 1;
 load_audio_start_second = 0;
-load_audio_end_second = 6; %-1 for whole file
+load_audio_end_second = 7; %-1 for whole file
 global output_directory="output_samples/";
 global ewls_lambda = 0.999;
 global ewls_lambda_0 = 0.998;
 global ewls_noise_variance_coupled = 1; % 1=coupled | other=decoupled
 global ewls_initial_cov_matrix = 100;
 global model_rank = 10;
-global mu = 4.5;
+global mu = 4;
 
 % Residual methods exclusive parameters
 block_size = 256;
 block_shift = 128;
 
 % Prediction methods exclusive parameters
-global max_corrupted_block_length = 200;
+global max_corrupted_block_length = 50;
 global detection_delay = 10*model_rank;
 global decimal_accuracy = 12;
-global alarm_expand = 2;
 
 %%% Reading input samples
 filenames = [ ...
@@ -40,38 +39,38 @@ filenames = [ ...
               "Chopin_Etiuda_Op_25_nr_10.WAV";...
               "12.wav" ...
             ]; 
-current_file = filenames(4,:);
+current_file = filenames(1,:);
 
 [input_signal, frequency] = load_audio(current_file, load_audio_start_second, load_audio_end_second);
 input_signal = input_signal(:,1);
 %%% Executing alogorithms
 
-%dbstop("R_U_S_T_RK"); 
-%[  R_U_S_T_RK_output_signal,...
-%   R_U_S_T_RK_detection_signal,...
-%   R_U_S_T_RK_residual_errors,...
-%   R_U_S_T_RK_activate_threshold,...
-%   R_U_S_T_RK_release_threshold  ] = R_U_S_T_RK(input_signal, block_size, block_shift);
-%R_U_S_T_RK_threshold = [R_U_S_T_RK_activate_threshold, R_U_S_T_RK_release_threshold];
-  
-dbstop("P_B_S_O_RK");  
-[  P_U_S_O_RK_output_signal,...
-   P_U_S_O_RK_detection_signal,...
-   P_U_S_O_RK_prediction_errors,...
-   P_U_S_O_RK_activate_threshold  ] = P_B_S_O_RK(input_signal);   
+
+[  R_U_S_T_RK_output_signal,...
+   R_U_S_T_RK_detection_signal,...
+   R_U_S_T_RK_residual_errors,...
+   R_U_S_T_RK_activate_threshold,...
+   R_U_S_T_RK_release_threshold  ] = R_U_S_T_RK(input_signal, block_size, block_shift);
+R_U_S_T_RK_threshold = [R_U_S_T_RK_activate_threshold, R_U_S_T_RK_release_threshold];
+
+[  P_B_S_O_RK_output_signal,...
+   P_B_S_O_RK_detection_signal,...
+   P_B_S_O_RK_prediction_errors,...
+   P_B_S_O_RK_activate_threshold  ] = P_B_S_O_RK(input_signal);   
+   
 %%% Plotting results 
 if should_plot
   %plot_result(1, input_signal, R_U_S_T_RK_detection_signal, R_U_S_T_RK_output_signal);
   %plot_error_detection(2, R_U_S_T_RK_residual_errors, R_U_S_T_RK_threshold);
 
   plot_result(3, input_signal, P_U_S_O_RK_detection_signal, P_U_S_O_RK_output_signal);
-  %plot_error_detection(4, P_U_S_O_RK_prediction_errors, P_U_S_O_RK_activate_threshold);
+  plot_error_detection(4, P_U_S_O_RK_prediction_errors, P_U_S_O_RK_activate_threshold);
 endif
 
 %%% Saving audio files
 if should_save_audio
   %save_audio(current_file, R_U_S_T_RK_output_signal, frequency, 1);
-  save_audio(current_file, "P_U_S_O_RK", P_U_S_O_RK_output_signal, frequency, 1);
+  save_audio(current_file, P_U_S_O_RK_output_signal, frequency, 1);
 endif
 
 
