c1bed7a067a461dd7e5bfc43a95a2def569d2072
Theyw <daw.weg95@gmail.com>
2020-08-23 20:35:57 +0200

    working on var stability

--- 02_model_stability/levinson_durbin_estimation.m
+++ 02_model_stability/levinson_durbin_estimation.m
@@ -1,11 +1,11 @@
 function [coefficients_estimate] = levinson_durbin_estimation (N, input_signal)
-  global process_rank;
-  input_signal = [zeros(process_rank,1); input_signal; zeros(process_rank,1)];
-  coefficients_estimate = zeros(process_rank, 1);
+  global model_rank;
+  input_signal = [zeros(model_rank,1); input_signal; zeros(model_rank,1)];
+  coefficients_estimate = zeros(model_rank, 1);
 
-  p = zeros(1, process_rank+1);
-  r = zeros(1, process_rank+1);
-  for k = 1:process_rank+1
+  p = zeros(1, model_rank+1);
+  r = zeros(1, model_rank+1);
+  for k = 1:model_rank+1
     for i = 1:N-k
       p(k) = p(k) + input_signal(end-i)*input_signal(end-i-k+1);
     endfor    
--- 02_model_stability/wwr_estimation.m
+++ 02_model_stability/wwr_estimation.m
@@ -1,3 +1,17 @@
-function [coefficients_estimate] = wwr_estimation (input1, input2)
+function [coefficients_estimate] = wwr_estimation (N, input_signal)
+  global model_rank;
+  input_signal = [zeros(model_rank,1); input_signal; zeros(model_rank,1)];
+  coefficients_estimate = zeros(model_rank, 1);
 
+  p = zeros(1, model_rank+1);
+  r = zeros(1, model_rank+1);
+  for k = 1:model_rank+1
+    for i = 1:N-k
+      p(k) = p(k) + input_signal(end-i)*input_signal(end-i-k+1);
+    endfor    
+    r(k) = (1/N)*p(k);
+  endfor  
+  R = build_toeplitz_matrix(r);
+  P = r(2:end);
+  coefficients_estimate = inv(R)*P';
 endfunction
