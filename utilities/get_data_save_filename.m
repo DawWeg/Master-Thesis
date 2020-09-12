9e3429803e42f4a49231a60d59d4e8e1f57f60f8
Maciej Sterma <maciej.sterma@gmail.com>
2020-09-12 20:24:40 +0200

    Fix extension

--- utilities/get_data_save_filename.m
+++ utilities/get_data_save_filename.m
@@ -1,9 +1,10 @@
 function [data_save_filename] = get_data_save_filename(method_name, ext)
   global output_directory input_filename;
+
+  id = "";
+  [dir, name, ext] = fileparts(input_filename);
   if nargin<2
     ext = ".dat"
   endif
-  id = "";
-  [dir, name, ext] = fileparts(input_filename);
   data_save_filename = strcat([output_directory, "data/",id, method_name, "_", name, ext]);
 endfunction
